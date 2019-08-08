10bfe99847e1966359bb32d113fbca4ce4dd3af2
prem <krishnasibani123@gmail.com>
2019-08-09 01:05:06 +0530

    Initial commit

---
+++ app/src/main/java/com/shivamkibhu/musicui/Main2Activity.java
@@ -0,0 +1,22 @@
+package com.shivamkibhu.musicui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        findViewById(R.id.search_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Activity.this, Main3Activity.class));
+            }
+        });
+    }
+}
